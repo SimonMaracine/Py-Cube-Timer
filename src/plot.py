e6e3364dccc5cb3d323121b7c34e99a6378ca9f6
SimonMaracine <simonmaracine@gmail.com>
2020-10-12 22:03:38 +0300

    Modify and improve plotting

--- src/plot.py
+++ src/plot.py
@@ -6,19 +6,19 @@ from src.session import SessionData
 def plot(session_data: SessionData):
     plt.figure().canvas.set_window_title("Statistics")  # This might be wrong, I'm not sure
 
-    solve_indices = [i for i in range(len(session_data.solves))]
     times = session_data.solves
+    solve_indices = [i + 1 for i in range(len(session_data.solves))]
     plt.plot(solve_indices, times, label="single", color="gray")
 
     ao5 = session_data.all_ao5
     if ao5:  # If it's not empty
         indices = [i + 5 for i in range(len(ao5))]
-        plt.plot(indices, ao5, label="ao5", color="blue")
+        plt.plot(indices, ao5, label="ao5", color="red")
 
     ao12 = session_data.all_ao12
     if ao12:  # If it's not empty
         indices = [i + 12 for i in range(len(ao12))]
-        plt.plot(indices, ao12, label="ao12", color="red")
+        plt.plot(indices, ao12, label="ao12", color="blue")
 
     plt.xlabel("solve index")
     plt.ylabel("solve time (s)")
