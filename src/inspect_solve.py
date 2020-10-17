e0ae4076e48713a7d2e3e25a9c875c15839091a3
SimonMaracine <simonmaracine@gmail.com>
2020-10-17 21:26:29 +0300

    Change inspect solve window a bit

--- src/inspect_solve.py
+++ src/inspect_solve.py
@@ -23,10 +23,10 @@ class InspectSolve(tk.Frame):
         tk.Label(self, text=solve.date, font="Times, 13").grid(row=3, column=0)
 
         self.frm_buttons = tk.Frame(self)
-        self.frm_buttons.grid(row=4, column=0)
+        self.frm_buttons.grid(row=4, column=0, pady=(12, 0))
 
         tk.Button(self.frm_buttons, text="Ok", command=self.top_level.destroy).grid(row=0, column=0)
-        tk.Button(self.frm_buttons, text="Delete", command=self.delete).grid(row=0, column=1, padx=(100, 0))
+        tk.Button(self.frm_buttons, text="Delete", command=self.delete, background="red").grid(row=0, column=1, padx=(100, 0))
 
     def delete(self):
         self.delete_solve(self.index)
